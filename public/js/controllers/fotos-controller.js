6725214092445cf5b16c05e59a600e42a31acb50
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2019-11-06 09:16:42 -0300

    Arquivos iniciais projeto

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+    $scope.foto = {
+        title: 'Ratinho',
+        url: 'https://img.ibxk.com.br/2019/07/11/11224741246144.jpg?w=1040'
+    };
+});
