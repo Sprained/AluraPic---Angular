a971c7e643178fac8f95979e2b67ffbd2d07073e
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2019-12-31 00:09:13 -0300

    Rotas Angular e otimização da mesma para HTML5

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
-angular.module('alurapic', ['diretivas']);
+angular.module('alurapic', ['diretivas', 'ngRoute']).config(function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/main.html',
+        controller: 'FotosController'
+    });
+
+    $routeProvider.when('/fotos/new', {
+        templateUrl: 'partials/foto.html'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/fotos' });
+});
